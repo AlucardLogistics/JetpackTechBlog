1ebfd088bbfe65cf2b4888250757b6fd2ffe1167
Alucard <alucardlogistics@gmail.com>
2020-11-12 16:02:13 -0500

    first commit

---
+++ app/src/main/java/com/logistics/alucard/jetpackarchitectureblog/ui/auth/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.logistics.alucard.jetpackarchitectureblog.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.logistics.alucard.jetpackarchitectureblog.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
