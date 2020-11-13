6e4d4cf32c4a68785fe40a93763fbdbd67af3119
Alucard <alucardlogistics@gmail.com>
2020-11-13 12:30:04 -0500

    added room database for the accounts

--- app/src/main/java/com/logistics/alucard/jetpackarchitectureblog/persistence/AppDataBase.kt
+++ app/src/main/java/com/logistics/alucard/jetpackarchitectureblog/persistence/AppDataBase.kt
@@ -1,5 +1,19 @@
 package com.logistics.alucard.jetpackarchitectureblog.persistence
 
-abstract class AppDataBase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.logistics.alucard.jetpackarchitectureblog.models.AccountProperties
+import com.logistics.alucard.jetpackarchitectureblog.models.AuthToken
+
+@Database(entities = [AuthToken::class, AccountProperties::class], version = 1)
+abstract class AppDataBase: RoomDatabase() {
+
+    abstract fun getAuthTokenDao(): AuthTokenDao
+
+    abstract fun getAccountPropertiesDao(): AccountPropertiesDao
+
+    companion object {
+        const val  DATABASE_NAME = "app_db"
+    }
 
 }
